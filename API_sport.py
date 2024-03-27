c8c7a190d1933bcdb8c348a4828f7377c419b620
BobLeClou <121858449+BobLeClou@users.noreply.github.com>
2024-03-27 15:17:58 +0100

    Update API_sport.py

--- API_sport.py
+++ API_sport.py
@@ -1,5 +1,4 @@
 from flask import Flask, render_template, request, jsonify
-import waitress  # pip install waitress : serveur WSGI pour Windows / pour Linux : gunicorn
 import json
 
 app = Flask(__name__)
